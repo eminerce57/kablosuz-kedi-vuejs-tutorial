4e363b93b188cdf7f49790fa92294154e52dea99
eminerce57 <83703865+eminerce57@users.noreply.github.com>
2023-03-14 16:14:06 +0300

    up

--- tutorial/1-vue-instance-template-attribute-event/app.js
+++ tutorial/1-vue-instance-template-attribute-event/app.js
@@ -1,8 +1,14 @@
 const app = Vue.createApp({
     data(){
         return{
-title :"my tags from vue"
-
+title :"my tags from vue",
+content:"lorem ipsum dolor sit from vue",
+array:[1,2,3,4],
+personal:{
+    name:"emin",
+    lname:"erce"
+},
+details:`<a target="_blank" href="https://eminerce.com">eminerce.com</a>`
         };
     }
 
