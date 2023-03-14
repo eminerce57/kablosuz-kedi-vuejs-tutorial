05122279a4ccaa811547e371f139e9e0fc962a50
eminerce57 <83703865+eminerce57@users.noreply.github.com>
2023-03-14 17:07:31 +0300

    update last template attribute event

--- tutorial/1-vue-instance-template-attribute-event/app.js
+++ tutorial/1-vue-instance-template-attribute-event/app.js
@@ -8,9 +8,28 @@ personal:{
     name:"emin",
     lname:"erce"
 },
-details:`<a target="_blank" href="https://eminerce.com">eminerce.com</a>`
-        };
-    }
+details:`<a target="_blank" href="https://eminerce.com">eminerce.com</a>`,
+url:"https://eminerce.com",
+divCordinat:"",
+inputValue:""
 
+        };
+    },
+methods: {
+    updateTitle(text) {
+        this.title= text || "change form js";
+    },
+    updatecordinate(e){
+        console.log(e.offsetX,"  ",e.offsetY)
+        this.divCordinat =`${e.offsetX}  ${e.offsetY}`
+        this.updateTitle(this.divCordinat)
+    },
+    updateValue(e){
+  
+        this.inputValue = e.target.value
+
+    }
+},
     
     }).mount("body")
+
