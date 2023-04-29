25a749626b156a3adc2f36dc32d7c3ad83af4289
Emin Erce <muhammeteminerce@gmail.com>
2023-04-29 20:55:20 +0300

    update css classlist project

--- tutorial/home-work/2-homeWork/app.js
+++ tutorial/home-work/2-homeWork/app.js
@@ -7,7 +7,6 @@ const app = Vue.createApp({
     methods: {
         showalert(){
             alert("hi")
-            
         }
     },
     })
--- tutorial/home-work/class-list-app/app.js
+++ tutorial/home-work/class-list-app/app.js
@@ -1,12 +1,27 @@
 const app = Vue.createApp({
-    data(){
-        return{
-         
+    data() {
+    return {
+        bgColor: false,
+        border: false,
+        borderRadius: false,
+        color: false,
+        fontBold: false,
+        fontItalic: false,
+    };
+    },
+    methods: {},
+    computed: {
+    textAreaClass() {
+        return {
+        bgColor: this.bgColor,
+        border: this.border,
+        borderRadius: this.borderRadius,
+        color: this.color,
+        fontBold: this.fontBold,
+        fontItalic: this.fontItalic,
         };
     },
-    methods: {
-      
     },
-    })
+});
 
-app.mount("body")
+app.mount("body");
